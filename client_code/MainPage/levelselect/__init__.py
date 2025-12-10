e088ffb6f0bed8ce03775e07fa4c560d490490bf
antoquefaith@gmail.com <antoquefaith@gmail.com>
2025-12-10 16:46:08 +0000

    Created MainPage.levelselect; Deleted MainPage.levelselection

--- client_code/MainPage/levelselect/__init__.py
+++ client_code/MainPage/levelselect/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import levelselectionTemplate
+from ._anvil_designer import levelselectTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class levelselection(levelselectionTemplate):
+class levelselect(levelselectTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
