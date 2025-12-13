de1ac57a35a6a35a27cded63647ce2bb9061dac4
antoquefaith@gmail.com <antoquefaith@gmail.com>
2025-12-13 11:46:33 +0000

    Edited StressLevelPage.stress_q3

--- client_code/StressLevelPage/stress_q3/__init__.py
+++ client_code/StressLevelPage/stress_q3/__init__.py
@@ -1,4 +1,4 @@
-# --- stress_q3.py (FINAL FIXED CODE) ---
+# --- stress_q3.py (FINAL VERIFIED CODE) ---
 
 from ._anvil_designer import stress_q3Template
 from anvil import *
@@ -31,7 +31,6 @@ class stress_q3(stress_q3Template):
       self.q3_next_btn.enabled = True # Enable the button
       return True
 
-      # --- FIX: This error block is now correctly outside the 'if valid:' block. ---
       # If we reach here, validation failed.
     self.label_error.text = result 
     self.label_error.visible = True
@@ -39,15 +38,12 @@ class stress_q3(stress_q3Template):
     return False
 
   def handle_input_and_advance(self):
-    # We rely on live_validate to check the final state before advancing
     if self.live_validate(): 
       # Data is valid, save it and advance
-      # Re-running validation just to get the final numeric result
       _, result = assessment_logic.validate_input(self.stress_q3_ans.text, 0, 16)
 
       assessment_logic.user_data['screen_time_hours'] = result
       open_form("StressLevelPage.stress_q4")
-      # No 'else' needed, as live_validate handles error display
 
   def q3_next_btn_click(self, **event_args):
     self.handle_input_and_advance()
@@ -55,7 +51,6 @@ class stress_q3(stress_q3Template):
   def stress_q3_ans_pressed_enter(self, **event_args):
     self.handle_input_and_advance()
 
-    # --- THIS IS THE NEW CHANGE EVENT HANDLER ---
   def stress_q3_ans_change(self, **event_args):
     """This method is called when the text in this text box is edited"""
     self.live_validate()
