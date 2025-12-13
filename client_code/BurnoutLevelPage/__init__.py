1c15aff6058587f09eb48573e122ee41c4bb1d98
antoquefaith@gmail.com <antoquefaith@gmail.com>
2025-12-13 04:29:05 +0000

    Created BurnoutLevelPage; Edited StressLevelPage.stress_q1, StressLevelPage.stress_q3, StressLevelPage and StressLevelPage.stress_q2; Deleted StressLevelPage.slidermodule

--- client_code/BurnoutLevelPage/__init__.py
+++ client_code/BurnoutLevelPage/__init__.py
@@ -1,15 +1,18 @@
-from ._anvil_designer import stress_q1Template
+from ._anvil_designer import BurnoutLevelPageTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class stress_q1(stress_q1Template):
+
+class BurnoutLevelPage(BurnoutLevelPageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def q1_next_btn_click(self, **event_args):
+    # Any code you write here will run before the form opens.
+
+  def stress_con_click(self, **event_args):
     open_form("StressLevelPage.stress_q1")
-  pass
+    pass
